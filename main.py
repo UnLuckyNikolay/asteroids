a5afcf9838eaa6c8c3386f1940840cc3872b5857
UnLuckyNikolay <nickslast@gmail.com>
2025-01-22 17:42:31 +0100

    Asteroid class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
 
 
 def main():
@@ -8,9 +9,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
