718302b0ca761b60e64c473b9c45adafc21cd287
UnLuckyNikolay <nickslast@gmail.com>
2025-01-22 18:14:13 +0100

    AsteroidField class, fixed Asteroid

--- asteroid.py
+++ asteroid.py
@@ -8,7 +8,7 @@ class Asteroid(CircleShape):
 
     
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.x, self.y, self.radius, 2)
+        pygame.draw.circle(screen, "white", (self.position.x, self.position.y), self.radius, 2)
 
     
     def update(self, dt):
